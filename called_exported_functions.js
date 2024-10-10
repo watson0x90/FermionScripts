1d81068a420b1c5224c84f50324bbdad97dba036
Watson <watson0x90@gmail.com>
2024-10-10 13:51:39 -0600

    Create called_exported_functions.js

---
+++ called_exported_functions.js
@@ -0,0 +1,28 @@
+// Check for exported functions called by an EXE.
+
+dllName = "example.dll"
+
+let dllModule = Process.findModuleByName(dllName);
+
+if (dllName) {
+    send("[+] " + dllName + " is loaded at base address: " + dllName.base);
+
+    // Enumerate all exported functions from the DLL using the correct method on the module object
+    let exports = dllName.enumerateExports();
+
+    send("[+] Hooking exported functions from "+dllName+":");
+    
+    // Hook each exported function
+    exports.forEach(function (exp) {
+        send("    |_ Hooking " + exp.name + " @ " + exp.address);
+
+        // Attach an interceptor to each exported function
+        Interceptor.attach(exp.address, {
+            onEnter: function (args) {
+                send("[+] Called function: " + exp.name);
+            }
+        });
+    });
+} else {
+    send("[-] "+dllName+" is not loaded.");
+}
