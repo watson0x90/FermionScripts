7142bac5f27848c3d2df0b0dba62599ce526e526
Watson <watson0x90@gmail.com>
2024-10-23 15:08:04 -0600

    Update called_exported_functions.js

--- called_exported_functions.js
+++ called_exported_functions.js
@@ -1,28 +1,33 @@
 // Check for exported functions called by an EXE.
 
-let dllName = "example.dll";
+let dllName = "KernelBase.dll";
 
 let dllModule = Process.findModuleByName(dllName);
 
-if (dllName) {
+if (dllModule) {
     send("[+] " + dllName + " is loaded at base address: " + dllModule.base);
 
-    // Enumerate all exported functions from the DLL using the correct method on the module object
+    // Enumerate all exported functions from the DLL
     let exports = dllModule.enumerateExports();
 
-    send("[+] Hooking exported functions from "+dllName+":");
+    send("[+] Hooking exported functions from " + dllName + ":");
     
     // Hook each exported function
     exports.forEach(function (exp) {
-        send("    |_ Hooking " + exp.name + " @ " + exp.address);
+        try {
+            send("    |_ Hooking " + exp.name + " @ " + exp.address);
 
-        // Attach an interceptor to each exported function
-        Interceptor.attach(exp.address, {
-            onEnter: function (args) {
-                send("[+] Called function: " + exp.name);
-            }
-        });
+            // Attach an interceptor to each exported function
+            Interceptor.attach(exp.address, {
+                onEnter: function (args) {
+                    send("[+] Called function: " + exp.name);
+                }
+            });
+        } catch (err) {
+            // If an error occurs, log it and continue with the next function
+            send("[-] Failed to hook " + exp.name + ": " + err.message);
+        }
     });
 } else {
-    send("[-] "+dllName+" is not loaded.");
+    send("[-] " + dllName + " is not loaded.");
 }
