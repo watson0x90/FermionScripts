42b4f1778cb2f69e0116892a5b62f5862492f1a9
Watson <watson0x90@gmail.com>
2024-10-24 10:39:37 -0600

    Update bcrypt_BCryptDecrypt.js

--- bcrypt_BCryptDecrypt.js
+++ bcrypt_BCryptDecrypt.js
@@ -5,31 +5,30 @@ Interceptor.attach(bcryptdecrypt, {
     onEnter: function(args) {
         this.plaintextPointer = args[6];
         this.plaintextSizeVal = args[7];
-        if (this.plaintextPointer.isNull()) {
-            this.abort = true;
-            return;
-        }
 
         try {
-            this.plaintextSize = this.plaintextSizeVal.readU64();
+            if(!this.plaintextPointer.isNull()){
+                this.plaintextSize = this.plaintextSizeVal.readU64();
+            }else{
+                // Used to know if we should attempt to read or not OnLeave
+                this.plaintextSize = 0;
+            }
         } catch (err) {
             //Enable for Debugging purposes
             //send('Error in onEnter: ' + err);
         }
     },
     onLeave: function(retval) {
-        if (this.abort || this.plaintextSize == 0) {
-            return;
-        }
-
-        try {
-            let plaintext = this.plaintextPointer.readCString(this.plaintextSize);
-            if (plaintext != null) {
-                send('Obtained cleartext is: ' + plaintext);
+        if (this.plaintextSize != 0) {
+            try {
+                let plaintext = this.plaintextPointer.readCString(this.plaintextSize);
+                if (plaintext != null) {
+                    send('[##] PlainText: \n\n' + plaintext);
+                }
+            } catch (err) {
+                //Enable for Debugging purposes
+                //send('Error in onLeave: ' + err);
             }
-        } catch (err) {
-            //Enable for Debugging purposes
-            //send('Error in onLeave: ' + err);
         }
     }
 });
