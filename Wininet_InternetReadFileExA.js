4b0d3860ba97d74a1fb6d7bdf1ac4d400905568f
Watson <watson0x90@gmail.com>
2024-10-25 20:57:36 -0600

    Update Wininet_InternetReadFileExA.js

--- Wininet_InternetReadFileExA.js
+++ Wininet_InternetReadFileExA.js
@@ -1,25 +1,25 @@
-var internetReadFileExA = Module.getExportByName("Wininet.dll", "InternetReadFileExA");
+const internetReadFileExA = Module.getExportByName("Wininet.dll", "InternetReadFileExA");
 
-Interceptor.attach(internetReadFileExA, {
+let wininetListener = Interceptor.attach(internetReadFileExA, {
     onEnter: function(args) {
         // Capture arguments of InternetReadFileExA
-        this.hInternet = args[0]; // HINTERNET handle
-        this.lpBuffersOut = args[1]; // LPINTERNET_BUFFERSA structure
-        this.dwFlags = args[2]; // Flags
+        let hInternet = args[0]; // HINTERNET handle
+        let lpBuffersOut = args[1]; // LPINTERNET_BUFFERSA structure
+        let dwFlags = args[2]; // Flags
 
         // Log basic information about the call
         send("[##] (OnEnter) InternetReadFileExA called");
-        send("    hInternet: " + this.hInternet);
-        send("    lpBuffersOut: " + this.lpBuffersOut);
-        send("    dwFlags: " + this.dwFlags);
+        send("    hInternet: " + hInternet);
+        send("    lpBuffersOut: " + lpBuffersOut);
+        send("    dwFlags: " + dwFlags);
         
         // Log more details if available
         try {
-            if (!this.lpBuffersOut.isNull()) {
+            if (!lpBuffersOut.isNull()) {
                 // Assuming lpBuffersOut points to an INTERNET_BUFFERSA structure
-                var bufferStruct = this.lpBuffersOut.add(8); // Adjust if structure is different
-                var bufferLength = bufferStruct.readUInt();  // Assuming length is at offset 8
-                var buffer = this.lpBuffersOut.add(12).readPointer();  // Assuming pointer is at offset 12
+                let bufferStruct = lpBuffersOut.add(8); // Adjust if structure is different
+                let bufferLength = bufferStruct.readUInt();  // Assuming length is at offset 8
+                let buffer = lpBuffersOut.add(12).readPointer();  // Assuming pointer is at offset 12
                 send("    Buffer length: " + bufferLength);
                 send("    Buffer address: " + buffer);
             }
@@ -30,5 +30,6 @@ Interceptor.attach(internetReadFileExA, {
     onLeave: function(retval) {
         // Log return value
         send("[##] (OnLeave) InternetReadFileExA returned: " + retval);
+        wininetListener.detach();
     }
 });
